179aaedd5dfffa51223af327af8a52bfa25dbeea
your <you>
2022-10-07 01:55:08 +0300

    fix

--- node/index.js
+++ node/index.js
@@ -8,17 +8,17 @@ const puppeteer = require('puppeteer');
 (async () => {
 	const browser = await puppeteer.launch({ headless: true, devtools: false });
 	const page = await browser.newPage();
-	const artifactsDir = process.env.ARTIFACTS_DIR;
-	await page._client().send('Page.setDownloadBehavior', {behavior: 'allow', downloadPath: path.resolve(artifactsDir)});
+	await page._client().send('Page.setDownloadBehavior', {behavior: 'allow', downloadPath: path.resolve('bin')});
 	await page.goto('https://www.eufic.org/en/explore-seasonal-fruit-and-vegetables-in-europe');
 	let [fruitString, vegetableString] = await page.evaluate(() => {
+		const Fruit = document.getElementById('Fruit');
 		let fruitNodeList = Fruit.childNodes[0].childNodes;
 		let fruitObject = {};
 		for (let i = 0; i < fruitNodeList.length; i++) {
 			let fruitArray = fruitNodeList[i].className;
 			fruitArray = fruitArray.split(' ');
 			fruitArray = fruitArray.filter(x => x.includes('-'));
-			for (j = 0; j < fruitArray.length; j++) {
+			for (let j = 0; j < fruitArray.length; j++) {
 				if (fruitObject[fruitArray[j]] === undefined) {
 					fruitObject[fruitArray[j]] = [fruitNodeList[i].id];
 				} else {
@@ -26,13 +26,14 @@ const puppeteer = require('puppeteer');
 				}
 			}
 		}
+		const Vegetable = document.getElementById('Vegetable');
 		let vegetableNodeList = Vegetable.childNodes[0].childNodes;
 		let vegetableObject = {};
 		for (let i = 0; i < vegetableNodeList.length; i++) {
 			let vegetableArray = vegetableNodeList[i].className;
 			vegetableArray = vegetableArray.split(' ');
 			vegetableArray = vegetableArray.filter(x => x.includes('-'));
-			for (j = 0; j < vegetableArray.length; j++) {
+			for (let j = 0; j < vegetableArray.length; j++) {
 				if (vegetableObject[vegetableArray[j]] === undefined) {
 					vegetableObject[vegetableArray[j]] = [vegetableNodeList[i].id];
 				} else {
@@ -44,8 +45,8 @@ const puppeteer = require('puppeteer');
 		vegetableString = JSON.stringify(vegetableObject);
 		return [fruitString, vegetableString];
 	});
-	fs.writeFileSync('release/fruit-english.js', `const fruitJsonString = \'${fruitString}\';`);
-	fs.writeFileSync('release/vegetable-english.js', `const vegetableJsonString = \'${vegetableString}\';`);
+	fs.writeFileSync('dist/fruit-english.js', `const fruitJsonString = '${fruitString}';`);
+	fs.writeFileSync('dist/vegetable-english.js', `const vegetableJsonString = '${vegetableString}';`);
 	const fruitGreekMap = JSON.parse(fs.readFileSync('fruit-english-to-greek.json', 'utf8'));
 	let fruitGreekString = fruitString;
 	for (const property in fruitGreekMap) {
@@ -56,8 +57,8 @@ const puppeteer = require('puppeteer');
 	for (const property in vegetableGreekMap) {
 		vegetableGreekString = vegetableGreekString.replaceAll(property, vegetableGreekMap[property]);
 	}
-	fs.writeFileSync('release/fruit-greek.js', `const fruitGreekJsonString = \'${fruitGreekString}\';`);
-	fs.writeFileSync('release/vegetable-greek.js', `const vegetableGreekJsonString = \'${vegetableGreekString}\';`);
+	fs.writeFileSync('dist/fruit-greek.js', `const fruitGreekJsonString = '${fruitGreekString}';`);
+	fs.writeFileSync('dist/vegetable-greek.js', `const vegetableGreekJsonString = '${vegetableGreekString}';`);
 	await page.close();
 	await browser.close();
 })();
