55c0e16eb03ad1a54b98b2b694a88e647c318857
Paschalis Bizopoulos <pbizopoulos@protonmail.com>
2022-11-21 01:27:25 +0200

    fix

---
+++ python/main.py
@@ -0,0 +1,44 @@
+from playwright.sync_api import sync_playwright
+import json
+
+
+def main():
+    with sync_playwright() as playwright:
+        browser = playwright.chromium.launch()
+        page = browser.new_page()
+        page.on('pageerror', lambda exception: (_ for _ in ()).throw(Exception(f'uncaught exception: {exception}')))
+        timeout = 100000
+        page.set_default_navigation_timeout(timeout)
+        page.set_default_timeout(timeout)
+        page.goto('https://www.eufic.org/en/explore-seasonal-fruit-and-vegetables-in-europe')
+        fruit_child_list = page.query_selector('#Fruit > .fvgrid').query_selector_all('xpath=child::*')
+        fruit_to_month_country_dict = dict()
+        for fruit_child in fruit_child_list:
+            fruit = fruit_child.query_selector('strong').inner_text().replace(' ', '-').replace('(', '').replace(')', '')
+            if fruit_child.get_attribute('class'):
+                month_country_list = fruit_child.get_attribute('class').split()
+                fruit_to_month_country_dict[fruit] = [month_country for month_country in month_country_list if '-' in month_country]
+        month_country_to_fruit_dict = dict()
+        for fruit in fruit_to_month_country_dict:
+            for month_country in fruit_to_month_country_dict[fruit]:
+                month_country_to_fruit_dict.setdefault(month_country, []).append(fruit)
+        with open('dist/month-country-to-fruit.json', 'w') as file:
+            file.write(f'const fruitObject = {json.dumps(month_country_to_fruit_dict)};')
+        vegetable_child_list = page.query_selector('#Vegetable > .fvgrid').query_selector_all('xpath=child::*')
+        vegetable_to_month_country_dict = dict()
+        for vegetable_child in vegetable_child_list:
+            vegetable = vegetable_child.query_selector('strong').inner_text().replace(' ', '-').replace('(', '').replace(')', '')
+            if vegetable_child.get_attribute('class'):
+                month_country_list = vegetable_child.get_attribute('class').split()
+                vegetable_to_month_country_dict[vegetable] = [month_country for month_country in month_country_list if '-' in month_country]
+        month_country_to_vegetable_dict = dict()
+        for vegetable in vegetable_to_month_country_dict:
+            for month_country in vegetable_to_month_country_dict[vegetable]:
+                month_country_to_vegetable_dict.setdefault(month_country, []).append(vegetable)
+        with open('dist/month-country-to-vegetable.json', 'w') as file:
+            file.write(f'const vegetableObject = {json.dumps(month_country_to_vegetable_dict)};')
+        browser.close()
+
+
+if __name__ == '__main__':
+    main()
--- script.js
+++ script.js
@@ -1,49 +1,11 @@
 'use strict';
-const countrySpan = document.getElementById('country-span');
 const countryValueSpan = document.getElementById('country-value-span');
 const dateCurrent = new Date();
 const fruitSeasonalUl = document.getElementById('fruit-seasonal-ul');
-const fruitSpan = document.getElementById('fruit-span');
-const homePageUrl = document.getElementById('home-page-url');
-const languageSelect = document.getElementById('language-select');
-const languageSpan = document.getElementById('language-span');
 const monthArray = ['january', 'february', 'march', 'april', 'may', 'june', 'july', 'august', 'september', 'october', 'november', 'december'];
 const monthCurrentValueSpan = document.getElementById('month-current-value-span');
-const monthGreekArray = ['ιανουάριος', 'φεβρουάριος', 'μάρτιος', 'απρίλιος', 'μάϊος', 'ιούνιος', 'ιούλιος', 'αύγουστος', 'σεπτέμβριος', 'οκτώβριος', 'νοέμβριος', 'δεκέμβριος'];
-const monthSpan = document.getElementById('month-span');
-const seasonalFoodsInEurope = document.getElementById('seasonal-foods-in-europe');
-const sourceCodeUrl = document.getElementById('source-code-url');
 const vegetableSeasonalUl = document.getElementById('vegetable-seasonal-ul');
-const vegetableSpan = document.getElementById('vegetable-span');
 const monthCurrent = monthArray[dateCurrent.getMonth()];
-const countryEnglishToGreekObject = {
-	'austria': 'Αυστρία',
-	'belgium': 'Βέλγιο',
-	'bulgaria': 'Βουλγαρία',
-	'cyprus': 'Κύπρος',
-	'czech republic': 'Δημοκρατία της Τσεχίας',
-	'denmark': 'Δανία',
-	'estonia': 'Εσθονία',
-	'finland': 'Φινλανδία',
-	'france': 'Γαλλία',
-	'germany': 'Γερμανία',
-	'greece': 'Ελλάδα',
-	'hungary': 'Ουγγαρία',
-	'ireland': 'Ιρλανδία',
-	'italy': 'Ιταλία',
-	'latvia': 'Λετονία',
-	'lithuania': 'Λιθουανία',
-	'luxembourg': 'Λουξεμβούργο',
-	'malta': 'Μάλτα',
-	'poland': 'Πολωνία',
-	'portugal': 'Πορτογαλία',
-	'slovenia': 'Σλοβενία',
-	'spain': 'Ισπανία',
-	'sweden': 'Σουηδία',
-	'turkey': 'Τουρκία',
-	'netherlands': 'Ολλανδία',
-	'unitedkingdom': 'Ηνωμένο Βασίλειο'
-};
 const timeZoneToCountryObject = {
 	'Europe/Vienna': 'austria',
 	'Europe/Brussels': 'belgium',
@@ -72,63 +34,28 @@ const timeZoneToCountryObject = {
 	'Europe/Amsterdam': 'netherlands',
 	'Europe/London': 'unitedkingdom'
 };
+
+function capitalizeFirstLetter(string) {
+	return string.charAt(0).toUpperCase() + string.slice(1);
+}
 let timeZoneCurrent = Intl.DateTimeFormat().resolvedOptions().timeZone;
 if (timeZoneToCountryObject[timeZoneCurrent] === undefined) {
 	timeZoneCurrent = 'Europe/London';
 }
 const countryCurrent = timeZoneToCountryObject[timeZoneCurrent];
-if (countryCurrent === 'greece') {
-	languageSelect.value = 'greek';
-} else {
-	languageSelect.value = 'english';
-}
-
-function capitalizeFirstLetter(string) {
-	return string.charAt(0).toUpperCase() + string.slice(1);
-}
-languageSelect.onchange = function() {
-	let fruitObject;
-	let vegetableObject;
-	if (this.value === 'greek') {
-		fruitObject = JSON.parse(fruitGreekJsonString);
-		vegetableObject = JSON.parse(vegetableGreekJsonString);
-		countrySpan.textContent = 'Χώρα';
-		countryValueSpan.textContent = capitalizeFirstLetter(countryEnglishToGreekObject[countryCurrent]);
-		fruitSpan.textContent = 'Φρούτα';
-		homePageUrl.textContent = 'Αρχική σελίδα';
-		languageSpan.textContent = 'Γλώσσα';
-		monthCurrentValueSpan.textContent = capitalizeFirstLetter(monthGreekArray[dateCurrent.getMonth()]);
-		monthSpan.textContent = 'Μήνας';
-		seasonalFoodsInEurope.textContent = 'Εποχιακά τρόφιμα στην Ευρώπη';
-		sourceCodeUrl.textContent = 'Πηγαίος κώδικας';
-		vegetableSpan.textContent = 'Λαχανικά';
-	} else {
-		fruitObject = JSON.parse(fruitJsonString);
-		vegetableObject = JSON.parse(vegetableJsonString);
-		countrySpan.textContent = 'Country';
-		countryValueSpan.textContent = capitalizeFirstLetter(countryCurrent);
-		fruitSpan.textContent = 'Fruits';
-		homePageUrl.textContent = 'Home page';
-		languageSpan.textContent = 'Language';
-		monthCurrentValueSpan.textContent = capitalizeFirstLetter(monthArray[dateCurrent.getMonth()]);
-		monthSpan.textContent = 'Month';
-		seasonalFoodsInEurope.textContent = 'Seasonal foods in Europe';
-		sourceCodeUrl.textContent = 'Source code';
-		vegetableSpan.textContent = 'Vegetables';
-	}
-	const fruitSeasonal = fruitObject[`${monthCurrent}-${countryCurrent}`];
-	const vegetableSeasonal = vegetableObject[`${monthCurrent}-${countryCurrent}`];
-	fruitSeasonalUl.innerHTML = '';
-	fruitSeasonal.forEach(function(item) {
-		let li = document.createElement('li');
-		fruitSeasonalUl.appendChild(li);
-		li.textContent += item;
-	});
-	vegetableSeasonalUl.innerHTML = '';
-	vegetableSeasonal.forEach(function(item) {
-		let li = document.createElement('li');
-		vegetableSeasonalUl.appendChild(li);
-		li.textContent += item;
-	});
-};
-languageSelect.onchange();
+countryValueSpan.textContent = capitalizeFirstLetter(countryCurrent);
+monthCurrentValueSpan.textContent = capitalizeFirstLetter(monthArray[dateCurrent.getMonth()]);
+const fruitSeasonal = fruitObject[`${monthCurrent}-${countryCurrent}`];
+const vegetableSeasonal = vegetableObject[`${monthCurrent}-${countryCurrent}`];
+fruitSeasonalUl.innerHTML = '';
+fruitSeasonal.forEach(function(item) {
+	let li = document.createElement('li');
+	fruitSeasonalUl.appendChild(li);
+	li.textContent += item;
+});
+vegetableSeasonalUl.innerHTML = '';
+vegetableSeasonal.forEach(function(item) {
+	let li = document.createElement('li');
+	vegetableSeasonalUl.appendChild(li);
+	li.textContent += item;
+});
