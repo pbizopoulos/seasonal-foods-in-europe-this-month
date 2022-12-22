f56eb8bcb63062b6dfb67ef1c8ac9a50c85bc18b
Paschalis Bizopoulos <pbizopoulos@protonmail.com>
2022-12-22 14:05:22 +0200

    fix

--- python/main.py
+++ python/main.py
@@ -1,5 +1,6 @@
-from playwright.sync_api import sync_playwright
 import json
+
+from playwright.sync_api import sync_playwright
 
 
 def main():
@@ -9,26 +10,26 @@ def main():
         page.on('pageerror', lambda exception: (_ for _ in ()).throw(Exception(f'uncaught exception: {exception}')))
         page.goto('https://www.eufic.org/en/explore-seasonal-fruit-and-vegetables-in-europe')
         fruit_child_list = page.query_selector('#Fruit > .fvgrid').query_selector_all('xpath=child::*')
-        fruit_to_month_country_dict = dict()
+        fruit_to_month_country_dict = {}
         for fruit_child in fruit_child_list:
             fruit = fruit_child.query_selector('strong').inner_text().replace(' ', '-').replace('(', '').replace(')', '')
             if fruit_child.get_attribute('class'):
                 month_country_list = fruit_child.get_attribute('class').split()
                 fruit_to_month_country_dict[fruit] = [month_country for month_country in month_country_list if '-' in month_country]
-        month_country_to_fruit_array_dict = dict()
+        month_country_to_fruit_array_dict = {}
         for fruit in fruit_to_month_country_dict:
             for month_country in fruit_to_month_country_dict[fruit]:
                 month_country_to_fruit_array_dict.setdefault(month_country, []).append(fruit)
         with open('dist/month-country-to-fruit-array.json', 'w', encoding='utf-8') as file:
             file.write(f'const monthCountryToFruitArrayObject = {json.dumps(month_country_to_fruit_array_dict)};')
         vegetable_child_list = page.query_selector('#Vegetable > .fvgrid').query_selector_all('xpath=child::*')
-        vegetable_to_month_country_dict = dict()
+        vegetable_to_month_country_dict = {}
         for vegetable_child in vegetable_child_list:
             vegetable = vegetable_child.query_selector('strong').inner_text().replace(' ', '-').replace('(', '').replace(')', '')
             if vegetable_child.get_attribute('class'):
                 month_country_list = vegetable_child.get_attribute('class').split()
                 vegetable_to_month_country_dict[vegetable] = [month_country for month_country in month_country_list if '-' in month_country]
-        month_country_to_vegetable_array_dict = dict()
+        month_country_to_vegetable_array_dict = {}
         for vegetable in vegetable_to_month_country_dict:
             for month_country in vegetable_to_month_country_dict[vegetable]:
                 month_country_to_vegetable_array_dict.setdefault(month_country, []).append(vegetable)
