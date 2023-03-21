1b5b638145c5411f3da70105af87feae46e1ec50
Paschalis Bizopoulos <pbizopoulos@protonmail.com>
2023-03-21 16:11:57 +0200

    .

--- python/main.py
+++ python/main.py
@@ -6,14 +6,14 @@ from playwright.sync_api import Error, Page, sync_playwright
 
 
 def get_month_country_to_food(page: Page, selector: str) -> str:
-    food_children = page.query_selector(selector).query_selector_all('xpath=child::*') # type: ignore[union-attr]
+    food_children = page.query_selector(selector).query_selector_all("xpath=child::*") # type: ignore[union-attr]
     food_to_month_country = {}
     for food_child in food_children:
-        food = food_child.query_selector('strong').inner_text().replace(' ', '-').replace('(', '').replace(')', '') # type: ignore[union-attr]
-        food_children_string = food_child.get_attribute('class')
+        food = food_child.query_selector("strong").inner_text().replace(" ", "-").replace("(", "").replace(")", "") # type: ignore[union-attr]
+        food_children_string = food_child.get_attribute("class")
         if food_children_string:
             months_countries = food_children_string.split()
-            food_to_month_country[food] = [month_country for month_country in months_countries if '-' in month_country]
+            food_to_month_country[food] = [month_country for month_country in months_countries if "-" in month_country]
     month_country_to_foods: dict = {} # type: ignore[type-arg]
     for food in food_to_month_country:
         for month_country in food_to_month_country[food]:
@@ -24,16 +24,16 @@ def main() -> None:
     with sync_playwright() as playwright:
         browser = playwright.chromium.launch()
         page = browser.new_page()
-        page.on('pageerror', page_error)
-        page.goto('https://www.eufic.org/en/explore-seasonal-fruit-and-vegetables-in-europe')
-        month_country_to_fruits = get_month_country_to_food(page, '#Fruit > .fvgrid')
-        with Path('dist/month-country-to-fruits.json').open('w', encoding='utf-8') as file:
-            file.write(f'const monthCountryToFruitArrayObject = {month_country_to_fruits};')
-        assert sha256(month_country_to_fruits.encode('utf-8')).hexdigest() == '044e94349d8dce547caee155a1e9e93158321a78dcb8252daa1beba3e9ae9920'
-        month_country_to_vegetables = get_month_country_to_food(page, '#Vegetable > .fvgrid')
-        with Path('dist/month-country-to-vegetables.json').open('w', encoding='utf-8') as file:
-            file.write(f'const monthCountryToVegetableArrayObject = {month_country_to_vegetables};')
-        assert sha256(month_country_to_vegetables.encode('utf-8')).hexdigest() == '47c61adc33f3f33e51805930433759110d67327eb414b003da35ac8331255dcb'
+        page.on("pageerror", page_error)
+        page.goto("https://www.eufic.org/en/explore-seasonal-fruit-and-vegetables-in-europe")
+        month_country_to_fruits = get_month_country_to_food(page, "#Fruit > .fvgrid")
+        with Path("dist/month-country-to-fruits.json").open("w", encoding="utf-8") as file:
+            file.write(f"const monthCountryToFruitArrayObject = {month_country_to_fruits};")
+        assert sha256(month_country_to_fruits.encode("utf-8")).hexdigest() == "044e94349d8dce547caee155a1e9e93158321a78dcb8252daa1beba3e9ae9920"
+        month_country_to_vegetables = get_month_country_to_food(page, "#Vegetable > .fvgrid")
+        with Path("dist/month-country-to-vegetables.json").open("w", encoding="utf-8") as file:
+            file.write(f"const monthCountryToVegetableArrayObject = {month_country_to_vegetables};")
+        assert sha256(month_country_to_vegetables.encode("utf-8")).hexdigest() == "47c61adc33f3f33e51805930433759110d67327eb414b003da35ac8331255dcb"
         browser.close()
 
 
@@ -41,5 +41,5 @@ def page_error(exception: Error) -> None:
     raise exception
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
--- script.js
+++ script.js
@@ -1,38 +1,38 @@
-'use strict';
-const countryValueSpan = document.getElementById('country-value-span');
+"use strict";
+const countryValueSpan = document.getElementById("country-value-span");
 const dateCurrent = new Date();
-const fruitUl = document.getElementById('fruit-ul');
-const monthArray = ['january', 'february', 'march', 'april', 'may', 'june', 'july', 'august', 'september', 'october', 'november', 'december'];
-const monthCurrentValueSpan = document.getElementById('month-current-value-span');
-const vegetableUl = document.getElementById('vegetable-ul');
+const fruitUl = document.getElementById("fruit-ul");
+const monthArray = ["january", "february", "march", "april", "may", "june", "july", "august", "september", "october", "november", "december"];
+const monthCurrentValueSpan = document.getElementById("month-current-value-span");
+const vegetableUl = document.getElementById("vegetable-ul");
 const monthCurrent = monthArray[dateCurrent.getMonth()];
 const timeZoneToCountryObject = {
-	'Europe/Amsterdam': 'netherlands',
-	'Europe/Ankara': 'turkey',
-	'Europe/Athens': 'greece',
-	'Europe/Berlin': 'germany',
-	'Europe/Brussels': 'belgium',
-	'Europe/Budapest': 'hungary',
-	'Europe/Copenhagen': 'denmark',
-	'Europe/Dublin': 'ireland',
-	'Europe/Helsinki': 'finland',
-	'Europe/Lisbon': 'portugal',
-	'Europe/Ljubljana': 'slovenia',
-	'Europe/London': 'unitedkingdom',
-	'Europe/Luxembourg': 'luxembourg',
-	'Europe/Madrid': 'spain',
-	'Europe/Nicosia': 'cyprus',
-	'Europe/Paris': 'france',
-	'Europe/Prague': 'czech republic',
-	'Europe/Riga': 'latvia',
-	'Europe/Rome': 'italy',
-	'Europe/Sofia': 'bulgaria',
-	'Europe/Stockholm': 'sweden',
-	'Europe/Tallinn': 'estonia',
-	'Europe/Valleta': 'malta',
-	'Europe/Vienna': 'austria',
-	'Europe/Vilnius': 'lithuania',
-	'Europe/Warsaw': 'poland',
+	"Europe/Amsterdam": "netherlands",
+	"Europe/Ankara": "turkey",
+	"Europe/Athens": "greece",
+	"Europe/Berlin": "germany",
+	"Europe/Brussels": "belgium",
+	"Europe/Budapest": "hungary",
+	"Europe/Copenhagen": "denmark",
+	"Europe/Dublin": "ireland",
+	"Europe/Helsinki": "finland",
+	"Europe/Lisbon": "portugal",
+	"Europe/Ljubljana": "slovenia",
+	"Europe/London": "unitedkingdom",
+	"Europe/Luxembourg": "luxembourg",
+	"Europe/Madrid": "spain",
+	"Europe/Nicosia": "cyprus",
+	"Europe/Paris": "france",
+	"Europe/Prague": "czech republic",
+	"Europe/Riga": "latvia",
+	"Europe/Rome": "italy",
+	"Europe/Sofia": "bulgaria",
+	"Europe/Stockholm": "sweden",
+	"Europe/Tallinn": "estonia",
+	"Europe/Valleta": "malta",
+	"Europe/Vienna": "austria",
+	"Europe/Vilnius": "lithuania",
+	"Europe/Warsaw": "poland",
 };
 
 function capitalizeFirstLetter(string) {
@@ -41,22 +41,22 @@ function capitalizeFirstLetter(string) {
 
 let timeZoneCurrent = Intl.DateTimeFormat().resolvedOptions().timeZone;
 if (timeZoneToCountryObject[timeZoneCurrent] === undefined) {
-	timeZoneCurrent = 'Europe/London';
+	timeZoneCurrent = "Europe/London";
 }
 const countryCurrent = timeZoneToCountryObject[timeZoneCurrent];
 countryValueSpan.textContent = capitalizeFirstLetter(countryCurrent);
 monthCurrentValueSpan.textContent = capitalizeFirstLetter(monthArray[dateCurrent.getMonth()]);
 const fruitArray = monthCountryToFruitArrayObject[`${monthCurrent}-${countryCurrent}`];
 const vegetableArray = monthCountryToVegetableArrayObject[`${monthCurrent}-${countryCurrent}`];
-fruitUl.innerHTML = '';
+fruitUl.innerHTML = "";
 fruitArray.forEach(function (item) {
-	let fruitLi = document.createElement('li');
+	let fruitLi = document.createElement("li");
 	fruitUl.appendChild(fruitLi);
 	fruitLi.textContent += item;
 });
-vegetableUl.innerHTML = '';
+vegetableUl.innerHTML = "";
 vegetableArray.forEach(function (item) {
-	let vegetableLi = document.createElement('li');
+	let vegetableLi = document.createElement("li");
 	vegetableUl.appendChild(vegetableLi);
 	vegetableLi.textContent += item;
 });
