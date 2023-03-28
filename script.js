af9d5226920c03139cade8018ae79205683b94c5
Paschalis Bizopoulos <pbizopoulos@protonmail.com>
2023-03-28 20:59:56 +0300

    .

--- script.js
+++ script.js
@@ -2,41 +2,49 @@
 const countryValueSpan = document.getElementById("country-value-span");
 const dateCurrent = new Date();
 const fruitUl = document.getElementById("fruit-ul");
-const monthArray = ["january", "february", "march", "april", "may", "june", "july", "august", "september", "october", "november", "december"];
+const monthArray = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 const monthCurrentValueSpan = document.getElementById("month-current-value-span");
 const vegetableUl = document.getElementById("vegetable-ul");
 const monthCurrent = monthArray[dateCurrent.getMonth()];
 const timeZoneToCountryObject = {
-	"Europe/Amsterdam": "netherlands",
-	"Europe/Ankara": "turkey",
-	"Europe/Athens": "greece",
-	"Europe/Berlin": "germany",
-	"Europe/Brussels": "belgium",
-	"Europe/Budapest": "hungary",
-	"Europe/Copenhagen": "denmark",
-	"Europe/Dublin": "ireland",
-	"Europe/Helsinki": "finland",
-	"Europe/Lisbon": "portugal",
-	"Europe/Ljubljana": "slovenia",
-	"Europe/London": "unitedkingdom",
-	"Europe/Luxembourg": "luxembourg",
-	"Europe/Madrid": "spain",
-	"Europe/Nicosia": "cyprus",
-	"Europe/Paris": "france",
-	"Europe/Prague": "czech republic",
-	"Europe/Riga": "latvia",
-	"Europe/Rome": "italy",
-	"Europe/Sofia": "bulgaria",
-	"Europe/Stockholm": "sweden",
-	"Europe/Tallinn": "estonia",
-	"Europe/Valleta": "malta",
-	"Europe/Vienna": "austria",
-	"Europe/Vilnius": "lithuania",
-	"Europe/Warsaw": "poland",
+	"Europe/Amsterdam": "Netherlands",
+	"Europe/Ankara": "Turkey",
+	"Europe/Athens": "Greece",
+	"Europe/Berlin": "Germany",
+	"Europe/Brussels": "Belgium",
+	"Europe/Budapest": "Hungary",
+	"Europe/Copenhagen": "Denmark",
+	"Europe/Dublin": "Ireland",
+	"Europe/Helsinki": "Finland",
+	"Europe/Lisbon": "Portugal",
+	"Europe/Ljubljana": "Slovenia",
+	"Europe/London": "United Kingdom",
+	"Europe/Luxembourg": "Luxembourg",
+	"Europe/Madrid": "Spain",
+	"Europe/Nicosia": "Cyprus",
+	"Europe/Paris": "France",
+	"Europe/Prague": "Czech Republic",
+	"Europe/Riga": "Latvia",
+	"Europe/Rome": "Italy",
+	"Europe/Sofia": "Bulgaria",
+	"Europe/Stockholm": "Sweden",
+	"Europe/Tallinn": "Estonia",
+	"Europe/Valleta": "Malta",
+	"Europe/Vienna": "Austria",
+	"Europe/Vilnius": "Lithuania",
+	"Europe/Warsaw": "Poland",
 };
 
-function capitalizeFirstLetter(string) {
-	return string.charAt(0).toUpperCase() + string.slice(1);
+function getAvailableFoods(month, country, fruit_or_vegetable) {
+	const foods = [];
+	for (const food in fvlist[fruit_or_vegetable]) {
+		for (const [m, c] of fvlist[fruit_or_vegetable][food]) {
+			if (m === month && c === country) {
+				foods.push(food);
+			}
+		}
+	}
+	return foods;
 }
 
 let timeZoneCurrent = Intl.DateTimeFormat().resolvedOptions().timeZone;
@@ -44,10 +52,10 @@ if (timeZoneToCountryObject[timeZoneCurrent] === undefined) {
 	timeZoneCurrent = "Europe/London";
 }
 const countryCurrent = timeZoneToCountryObject[timeZoneCurrent];
-countryValueSpan.textContent = capitalizeFirstLetter(countryCurrent);
-monthCurrentValueSpan.textContent = capitalizeFirstLetter(monthArray[dateCurrent.getMonth()]);
-const fruitArray = monthCountryToFruitArrayObject[`${monthCurrent}-${countryCurrent}`];
-const vegetableArray = monthCountryToVegetableArrayObject[`${monthCurrent}-${countryCurrent}`];
+countryValueSpan.textContent = countryCurrent;
+monthCurrentValueSpan.textContent = monthArray[dateCurrent.getMonth()];
+const fruitArray = getAvailableFoods(monthCurrent, countryCurrent, "Fruit");
+const vegetableArray = getAvailableFoods(monthCurrent, countryCurrent, "Vegetable");
 fruitUl.innerHTML = "";
 fruitArray.forEach(function (item) {
 	let fruitLi = document.createElement("li");
